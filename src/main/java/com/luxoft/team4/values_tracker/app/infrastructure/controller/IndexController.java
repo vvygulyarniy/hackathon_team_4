bbaf8cf09ea69b309f35dff506e19f728f3d255c
yuriy.yermolenko <yuriy.yermolenko@gmail.com>
2016-11-26 01:13:47 +0100

    Added routing

--- src/main/java/com/luxoft/team4/values_tracker/app/infrastructure/controller/IndexController.java
+++ src/main/java/com/luxoft/team4/values_tracker/app/infrastructure/controller/IndexController.java
@@ -4,16 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 /**
  * Created by vadim.vygulyarniy
  */
 @Controller
-@RequestMapping(value = "/**")
 public final class IndexController {
 
-	@RequestMapping(method = GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getIndexFile() {
 		return "index";
 	}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,24 @@
-function setBarWidth(dataElement, barElement, cssProperty, barPercent) {
-  var listData = [];
-  $(dataElement).each(function() {
-    listData.push($(this).html());
-  });
-  var listMax = Math.max.apply(Math, listData);
-  $(barElement).each(function(index) {
-    $(this).css(cssProperty, (listData[index] / listMax) * barPercent + "%");
-  });
-}
-setBarWidth(".bars span", ".bars em", "padding-right", 50);
+var app = angular.module("myApp", ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider',function ($routeProvider, $locationProvider) {
+	$routeProvider
+	//home
+	
+	.when("/excelence", {templateUrl: "templates/valueTemplate.html", controller: "PageCtrl"})
+	.when("/creativity", {templateUrl: "templates/valueTemplate.html", controller: "PageCtrl"})
+	.when("/relationships", {templateUrl: "templates/valueTemplate.html", controller: "PageCtrl"})
+	.when("/agility", {templateUrl: "templates/valueTemplate.html", controller: "PageCtrl"})
+	.when("/fun", {templateUrl: "templates/valueTemplate.html", controller: "PageCtrl"})
+	.when("/commitment", {templateUrl: "templates/valueTemplate.html", controller: "PageCtrl"})
+	
+	.otherwise({templateUrl: "globalStats.html", controller: "PageCtrl"});
+	
+	$locationProvider.html5Mode({
+	  enabled: true,
+	  requireBase: false
+	});
+}]);
+
+app.controller('PageCtrl', function ( $scope, $location, $http) {
+	$scope.text = $location.url();
+});
